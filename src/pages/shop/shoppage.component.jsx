89463f0690b28ebb6bafa6c3dc4ed223f2af8752
9631179448 <46281598+9631179448@users.noreply.github.com>
2019-12-03 11:53:51 +0530

    shoppage  components

--- src/pages/shop/shoppage.component.jsx
+++ src/pages/shop/shoppage.component.jsx
@@ -1,20 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+
 import SHOP_DATA from './shop.data';
 
-export default class ShopPage extends Component {
-    constructor(props) {
-        super(props);
-            this.state =  {
-                collections: SHOP_DATA
-        }
-    } 
-    render() {
-        return(
-            <div>
-                ShopPage
-            </div>
-        )
-    }   
+import CollectionPreview from '../../components/preview-collection/collection-preview';
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 
-
+export default ShopPage;
