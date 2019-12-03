4e682fa7a4a0d150fb81f1b00ff0ffb90d4282f0
9631179448 <46281598+9631179448@users.noreply.github.com>
2019-12-03 12:20:52 +0530

    header  components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shoppage.component';
 
@@ -11,6 +12,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+    <Header />
       <Route exact path = '/' component = { HomePage } />
       <Route exact path = '/hats' component = { HatsPage } />
       <Route exact path = '/shop' component = { ShopPage } />
