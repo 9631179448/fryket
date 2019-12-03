61a05b6dd729700352880774779556bc379a10cc
9631179448 <46281598+9631179448@users.noreply.github.com>
2019-12-03 09:32:50 +0530

    shop  components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shoppage.component';
+
+const HatsPage = () => (
+  <div><h1>HatsPage</h1></div>
+)
 
 function App() {
   return (
-    <div className="App">
-        <HomePage />
-    </div>
+    <div>
+      <Route exact path = '/' component = { HomePage } />
+      <Route exact path = '/hats' component = { HatsPage } />
+      <Route exact path = '/shop' component = { ShopPage } />
+
+    </div> 
   );
 }
 
